869d2636838de707c89e08a6f31662d3b1ba50db
Hellen Santos <hellenn.s@outlook.com>
2023-07-19 11:01:51 -0300

    Resolucao de exercicio troco.cpp

---
+++ troco.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    double preco, dinheiro, troco;
+    int quantidade;
+
+    cout << "Preco unitário do produto: ";
+    cin >> preco;
+
+    cout << "Quantidade comprada: ";
+    cin >> quantidade;
+
+    cout << "Dinheiro recebido: ";
+    cin >> dinheiro;
+
+    troco = dinheiro - (preco * quantidade);
+
+    cout << fixed << setprecision(2);
+    cout << "TROCO = " << troco << endl;
+
+    return 0;
+}
