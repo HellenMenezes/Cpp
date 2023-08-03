53bff89163f71c97478f9938ff15790bbbf68fc2
Hellen Santos <hellenn.s@outlook.com>
2023-08-03 16:45:59 -0300

    Resolucao de exercicio aumento.cpp

---
+++ aumento.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    double salario, novoSalario, aumento;
+    int porcentagem;
+
+    cout << "Digite o salario da pessoa: ";
+    cin >> salario;
+
+    if(salario <= 1000)
+    {
+        porcentagem = 20;
+    }
+    else if(salario <= 3000){
+        porcentagem = 15;
+    }
+    else if(salario <= 8000){
+        porcentagem = 10;
+    }
+    else{
+        porcentagem = 5;
+    }
+
+    aumento = salario * porcentagem / 100;
+    novoSalario = salario + aumento;
+
+    cout << fixed << setprecision(2);
+
+    cout << "Novo salario = R$ " << novoSalario << endl;
+    cout << "Aumento = R$ " << aumento << endl;
+    cout << "Porcentagem = " << porcentagem << " %" << endl;
+
+    return 0;
+}
