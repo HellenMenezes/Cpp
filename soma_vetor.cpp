7ea712ef208224137d29f07c1c868e71f4931bec
Hellen Santos <hellenn.s@outlook.com>
2023-10-05 12:18:16 -0300

    Resolucao de exercicio soma_vetor.cpp

---
+++ soma_vetor.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    int n, i;
+    double soma = 0, media;
+
+    cout << "Quantos numeros voce vai digitar? ";
+    cin >> n;
+
+    while (n <= 0)
+    {
+        cout << "Digite um numero positivo para n: ";
+        cin >> n;
+    }
+
+    vector<double> vet(n);
+
+    for(i = 0; i < n; i++)
+    {
+        cout << "Digite um numero: ";
+        cin >> vet[i];
+    }
+
+    cout << fixed << setprecision(1);
+    cout << endl << "VALORES: ";
+
+    for(i = 0; i < n; i++)
+    {
+        cout << vet[i] << "  ";
+    }
+
+    for(i = 0; i < n; i++)
+    {
+        soma = soma + vet[i];
+    }
+
+    media = soma / n;
+    cout << fixed << setprecision(2);
+    cout << endl << "SOMA = " << soma << endl;
+    cout << "MEDIA = " << media << endl;
+
+    return 0;
+}
