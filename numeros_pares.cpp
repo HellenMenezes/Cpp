7f651e67aa088f17fcadfc3f098b5a697f9c0561
Hellen Santos <hellenn.s@outlook.com>
2023-10-05 13:42:26 -0300

    Resolucao de exercicio numeros_pares.cpp

---
+++ numeros_pares.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int n, cont = 0;
+
+    cout << "Quantos numeros voce vai digitar? ";
+    cin >> n;
+
+    vector<int> vet(n);
+
+    for(int i = 0; i < n; i++)
+    {
+        cout << "Digite um numero: ";
+        cin >> vet[i];
+    }
+
+    cout << endl << "NUMEROS PARES: " << endl;
+
+    for(int i = 0; i < n; i++)
+    {
+        if(vet[i] % 2 == 0)
+        {
+            cout << vet[i] << "  ";
+            cont++;
+        }
+    }
+
+    cout << endl << endl << "QUANTIDADE DE PARES = " << cont << endl;
+
+    return 0;
+}
